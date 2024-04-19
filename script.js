fef2594bbc4126a2964473c7a2a4f2189b572ed0
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-04-19 08:40:20 -0300

    switch player turn

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ const td = document.querySelectorAll('td') //seleciona todos os meus table data
 td.forEach(function (playerInputField){
     playerInputField.addEventListener('click', function (ev){
         const selectedField = ev.currentTarget
+        //captura os meus jogadores
+        const crossPlayer = document.querySelector('.Xturn')
+        const circlePlayer = document.querySelector('.Oturn')
         //verifica se o campo já está preenchido
         if(selectedField.innerText !== 'X' && selectedField.innerText !== 'O'){
             if(table.dataset.player === 'cross'){
                 selectedField.innerText = 'X'
                 selectedField.dataset.value = 'X'
+                //remove a classe active do X para ser adicionada no O
+                crossPlayer.classList.remove('active')
+                circlePlayer.classList.add('active')
                 table.dataset.player = 'circle'
             } else {
                 selectedField.innerText = 'O'
                 selectedField.dataset.value = 'O'
+                //remove a classe active do O para ser adicionada no X
+                crossPlayer.classList.add('active')
+                circlePlayer.classList.remove('active')
                 table.dataset.player = 'cross'
             }
         }
