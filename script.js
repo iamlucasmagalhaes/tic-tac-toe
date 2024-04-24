209be06bc741d1722283ebd5ea8d0d41d586a7d5
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-04-23 23:28:52 -0300

    restarting project

--- script.js
+++ script.js
@@ -1,60 +1,136 @@
-const table = document.querySelector('table') //seleciona a minha table
-const td = document.querySelectorAll('td') //seleciona todos os meus table data
-const firstRow = document.querySelectorAll('.firstRow td') //recebe todos os td da primeira linha
-const secondRow = document.querySelectorAll('.secondRow td') //recebe todos os td da segunda linha
-const thirdRow = document.querySelectorAll('.thirdRow td') //recebe todos os td da terceira linha
+const boardGame = []
+const width = 3
+const height = 3
 
-//verifica se todos os value da linha são iguais, se forem mostra o ganhador
-function checkRowWinner (indexOfRow){
-    let awnserRow = []
-    indexOfRow.forEach(function (checkValue){
-        const value = checkValue.dataset.value
-        if(value !== '' && value !== undefined && value !== null){
-            awnserRow.push(value)
-
-            if(awnserRow[0] === awnserRow[1] && awnserRow[0] === awnserRow[2]){
-                console.log('funcionou')
-            }
-        }
-    })
+function start(){
+    createBoardGameDataStructure()
+    renderBoardGame()
 }
 
-//percorre todos os meus table datas e atribui uma função para cada um
-td.forEach(function (playerInputField){
-    playerInputField.addEventListener('click', function (ev){
-        const selectedField = ev.currentTarget
-        //captura os meus jogadores
+//cria a estrutura do meu tabuleiro
+function createBoardGameDataStructure(){
+    for(let i = 0; i < width; i++){
+        boardGame[i] = []
+        for(let j = 0; j < height; j++){
+            boardGame[i][j] = ''
+        }
+    }
+
+    return boardGame
+}
+
+function renderBoardGame(){
+    const board = document.getElementById('board') //captura o meu tabuleira
+
+    for(let i = 0; i < width; i++){
+        const tr = document.createElement('tr') //cria uma nova linha a cada interação
+        for(let j = 0; j < height; j++){
+            const td = document.createElement('td') //cria um novo campo a cada interação
+            const index = (i * width) + j //calcula um indice para cada campo
+            td.innerText = boardGame[i][j] //atribui o valor da minha matriz ao tabuleiro
+            td.dataset.index = index //atribuindo um indice
+            td.dataset.row = i //adicona um valor para representar a linha
+            td.dataset.col = j //adiciona um valor para representar a coluna
+            td.addEventListener('click', handleClick)
+            tr.appendChild(td) //adiciona o campo a linha  
+        }
+        board.appendChild(tr) //adicona a linha criada ao meu tabuleiro
+    }
+}
+
+//função para tratar o evento de clique nos botões
+function handleClick(){
+    const table = document.querySelector('table')
+    const row = Number(this.dataset.row) //obtem a linha clicada
+    const col = Number(this.dataset.col) //obtem a coluna clicada
+    //verifica se o valor dentro do campo clicado é vazio
+    if(boardGame[row][col] === ''){
         const crossPlayer = document.querySelector('.Xturn')
         const circlePlayer = document.querySelector('.Oturn')
-        //verifica se o campo já está preenchido
-        if(selectedField.innerText !== 'X' && selectedField.innerText !== 'O'){
-            if(table.dataset.player === 'cross'){
-                selectedField.innerText = 'X'
-                selectedField.dataset.value = 'X'
-                //remove a classe active do X para ser adicionada no O
-                crossPlayer.classList.remove('active')
-                circlePlayer.classList.add('active')
-                
-                //chama a minha função, que vai receber o indice da linha como parâmetro para verificar se X é o gannhador
-                checkRowWinner(firstRow)
-                checkRowWinner(secondRow)
-                checkRowWinner(thirdRow)
+        if(table.dataset.player === 'cross'){
+            boardGame[row][col] = 1 //altera o valor
+            this.innerText = 'X' //muda o texto dentro da campo clicado
+            crossPlayer.classList.remove('active')
+            circlePlayer.classList.add('active')
+            table.dataset.player = 'circle' //muda o valor da tabela
+        } else if(table.dataset.player === 'circle'){
+            boardGame[row][col] = 0 //altera o valor
+            this.innerText = 'O' //muda o texto dentro da campo clicado
+            crossPlayer.classList.add('active')
+            circlePlayer.classList.remove('active')
+            table.dataset.player = 'cross' //muda o valor da tabela
+        }
+        checkWinner()
+    }
+}
 
-                table.dataset.player = 'circle'
-            } else {
-                selectedField.innerText = 'O'
-                selectedField.dataset.value = 'O'
-                //remove a classe active do O para ser adicionada no X
-                crossPlayer.classList.add('active')
-                circlePlayer.classList.remove('active')
+function checkWinner(){
+    //verifica a linha que ganhou
+    for(let i = 0; i < width; i++){
+        if(
+            boardGame[i][0] !== '' &&
+            boardGame[i][0] === boardGame[i][1] &&
+            boardGame[i][0] === boardGame[i][2]
+        ){
+            //alert('Jogador ' + (boardGame[i][0] === 1 ? 'X' : 'O') + ' ganhou')
+            const rowCells = document.querySelectorAll(`td[data-row="${i}"]`)
+            rowCells.forEach(cell => cell.classList.add('winner'))
+            return
+        }
+    }    
 
-                //chama a minha função, que vai receber o indice da linha como parâmetro para verificar se O é o gannhador
-                checkRowWinner(firstRow)
-                checkRowWinner(secondRow)
-                checkRowWinner(thirdRow)
+    //verifica a coluna que ganhou
+    for(let j = 0; j < height; j++){
+        if(
+            boardGame[0][j] !== '' &&
+            boardGame[0][j] === boardGame[1][j] &&
+            boardGame[0][j] === boardGame[2][j]
+        ){
+            //alert('Jogador ' + (boardGame[0][j] === 1 ? 'X' : 'O') + ' ganhou')
+            const colCells = document.querySelectorAll(`td[data-col="${j}"]`)
+            colCells.forEach(cell => cell.classList.add('winner'))
+            return
+        }
+    }
 
-                table.dataset.player = 'cross'
+    //verifica a diagonal principal
+    if (
+        boardGame[0][0] !== '' &&
+        boardGame[0][0] === boardGame[1][1] &&
+        boardGame[0][0] === boardGame[2][2]
+    ) {
+        const diagonalCells = document.querySelectorAll('td[data-row][data-col][data-row="0"][data-col="0"], td[data-row][data-col][data-row="1"][data-col="1"], td[data-row][data-col][data-row="2"][data-col="2"]');
+        diagonalCells.forEach(cell => cell.classList.add('winner'))
+        return
+    }
+
+    //verifica a diagonal secundária
+    if (
+        boardGame[0][2] !== '' &&
+        boardGame[0][2] === boardGame[1][1] &&
+        boardGame[0][2] === boardGame[2][0]
+    ) {
+        const diagonalCells = document.querySelectorAll('td[data-row][data-col][data-row="0"][data-col="2"], td[data-row][data-col][data-row="1"][data-col="1"], td[data-row][data-col][data-row="2"][data-col="0"]')
+        diagonalCells.forEach(cell => cell.classList.add('winner'))
+        return
+    }
+
+
+    if(checkDraw()){
+        alert('O jogo terminou em empate!')
+    }
+}
+
+//verifica se houve empate
+function checkDraw(){
+    for(let i = 0; i < width; i++){
+        for(let j = 0; j < height; j++){
+            if(boardGame[i][j] === ''){
+                return false
             }
         }
-    })
-})
+    }
+    return true
+}
+
+start()
